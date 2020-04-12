2887041c994d5cb9ad749f2d4c60b0c2ac596861
FuzedxPheonix <cristigu@umich.edu>
2020-04-12 15:30:31 -0400

    public react typescript leaflet draw map

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useEffect} from 'react';
+import L from "leaflet";
+import "leaflet-draw";
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    useEffect(() => {
+        const map = L.map("map").setView([43,
+            -83.37146759033203], 14);
+
+
+        L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
+            attribution:
+                '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+        }).addTo(map);
+
+
+
+
+        const drawControl = new L.Control.Draw({
+            position: "topleft"
+        });
+
+
+        const drawn_items = L.featureGroup([]).addTo(map);
+
+        const layer_group = L.featureGroup([]).addTo(map);
+        map.addControl(drawControl);
+
+        map.on(L.Draw.Event.CREATED, function (event) {
+            let layer = event.layer;
+            drawn_items.addLayer(layer);
+        });
+    })
+
+
+    return (
+        <div>
+            <div id="map"/>
+        </div>
+    );
 }
 
 export default App;
